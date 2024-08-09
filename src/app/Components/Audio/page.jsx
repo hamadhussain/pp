5a030b41e6bc2bfa8f0b25d37d0097d73d6104b6
hamadhussain <108784635+hamadhussain@users.noreply.github.com>
2024-08-09 16:55:16 +0500

    seccc

--- src/app/Components/Audio/page.jsx
+++ src/app/Components/Audio/page.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Page = ({children}) => {
-    const audio = new Audio("/audio/birds39-forest-20772.mp3");
-    audio.loop = true;
-    audio.play();
+    // const audio = new Audio("/audio/birds39-forest-20772.mp3");
+    // audio.loop = true;
+    // audio.play();
   return (
     <>
       {children}
