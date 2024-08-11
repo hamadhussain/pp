fb3be133f2d817c0db7185bb65021afe6768eae5
hamadhussain <108784635+hamadhussain@users.noreply.github.com>
2024-08-12 00:33:00 +0500

    ff

--- src/app/Components/About/Progess/page.jsx
+++ src/app/Components/About/Progess/page.jsx
@@ -1,16 +1,14 @@
-"use client"
-import React, { useState,useEffect } from 'react'
-import { Progress } from "@/components/ui/progress"
+"use client";
+import React, { useState, useEffect } from "react";
+import { Progress } from "@/components/ui/progress";
 
-const page = ({values}) => {
-  const [progress, setProgress] = useState(13)
+const Page = ({ values }) => {
+  const [progress, setProgress] = useState(13);
   useEffect(() => {
-    const timer = setTimeout(() => setProgress(values), 700)
-    return () => clearTimeout(timer)
-  }, [])
-  return (
-    <Progress value={progress} className="w-[100%]  " />
-  )
-}
+    const timer = setTimeout(() => setProgress(values), 700);
+    return () => clearTimeout(timer);
+  }, [values]);
+  return <Progress value={progress} className="w-[100%]  " />;
+};
 
-export default page
+export default Page;
